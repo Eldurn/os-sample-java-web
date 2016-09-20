033a9f67d682401edce67197e244882dcc38eb98
Eldurn <skatainen@gmail.com>
2016-09-20 18:00:07 +0300

    Update HelloWorld.java

--- src/main/webapp/HelloWorld.java
+++ src/main/webapp/HelloWorld.java
@@ -1,35 +1,37 @@
 package main.webapp;
 
-// Import required java libraries
-import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-// Extend HttpServlet class
+/**
+ * Servlet implementation class HelloWorld
+ */
 public class HelloWorld extends HttpServlet {
- 
-  private String message;
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
 
-  public void init() throws ServletException
-  {
-      // Do required initialization
-      message = "Hello World";
-  }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().println("Hello World!");
+	}
 
-  public void doGet(HttpServletRequest request,
-                    HttpServletResponse response)
-            throws ServletException, IOException
-  {
-      // Set response content type
-      response.setContentType("text/html");
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
 
-      // Actual logic goes here.
-      PrintWriter out = response.getWriter();
-      out.println("<h1>" + message + "</h1>");
-  }
-  
-  public void destroy()
-  {
-      // do nothing.
-  }
 }
